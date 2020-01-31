d35a3dc0542adc3981abc2662cebefc2a77b3e2e
Filip Loster <fblost@gmail.com>
2020-01-31 17:16:44 +0100

    Simple setup

---
+++ code/GGJ2020/Assets/Scripts/Msg/Msg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using TinyMessenger;
+using UnityEngine;
+
+public class Msg {
+
+  
+    /*
+    public class ShowScreenWorker : ITinyMessage {
+        private static ShowScreenWorker _Instance;
+
+        public Type ControllerClass;
+        public bool Animated;
+        public float RemainingAnimationTime;
+        public object[] ExtraValues;
+
+        #region Implementation
+        public static ShowScreenWorker Get(Type controllerClass, bool animated, float remainingAnimTime, object[] extra) {
+            if (_Instance == null)
+                _Instance = new ShowScreenWorker();
+
+            _Instance.ControllerClass = controllerClass;
+            _Instance.Animated = animated;
+            _Instance.RemainingAnimationTime = remainingAnimTime;
+            _Instance.ExtraValues = extra;
+
+            return _Instance;
+        }
+
+        public object Sender {
+            get { return null; }
+        }
+        #endregion        
+    }
+    */
+
+}
